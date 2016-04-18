1a41173adc071e4b1eca9d50e79ff3013134d098
Belka Anton <belka_anton@belka-antons-mac.local>
2016-04-18 11:00:57 +0300

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by Belka_Anton on 18.04.16.
+//  Copyright © 2016 Belka_Anton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
